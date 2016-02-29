00410161938c877394ecd2fe2603c4648959bf45
mwytock0812 <michael.wytock@gmail.com>
2016-02-29 12:51:57 -0500

    Add 'test' and 'build' tasks to gulpfile and refactor project and add configuration for nightwatch.

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,9 @@ var bodyParser = require('body-parser');
 // var source = require('vinyl-source-stream');
 // var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
-var Server = require('karma').Server;
+
+var mocha = require('gulp-mocha');
+var nightwatch = require('gulp-nightwatch');
 
 var config = {
   srcCss : 'viewer/css/**/*.css',
@@ -28,22 +30,24 @@ var config = {
   distJs : 'dist/viewer/js/'
 };
 
-// Copies html, builds javascript and css, runs jshint, watches for changes and runs a web server and opens default browser to preview.
-gulp.task('default', ['copy-html',
-                      'copy-js',
-                      'watch',
-                      'jshint',
-                      'build-js',
-                      'build-css',
-                      'proxy',
-                      'test'], function() {
-  // launch web server
+// Runs jshint, sets up proxy server, runs jshint, and then sets up static server.
+gulp.task('default', ['watch', 'jshint', 'proxy'], function() {
+  // launch static web server
   gulpConnect.server({
     port: 3000,
     root: 'viewer',
     livereload: true
   });
 });
+
+// Runs e2e tests with selenium, then runs unit tests with mocha.
+gulp.task('test', ['e2e-tests'], function () {
+  return gulp.src('test/unit/*.js', {read: false})
+    .pipe(mocha({reporter: 'spec'}));
+});
+
+// Copies html, builds javascript and css
+gulp.task('build', ['copy-html', 'copy-js', 'build-js', 'build-css']);
 
 gulp.task('proxy', function () {
   var proxy = connect();
@@ -101,11 +105,11 @@ gulp.task('build-js', function() {
     .pipe(gulp.dest(config.distJs));
 });
 
-gulp.task('test', function(done) {
-  new Server({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: true
-  }, done).start();
+gulp.task('e2e-tests', ['default'], function () {
+  return gulp.src('')
+    .pipe(nightwatch({
+      configFile: 'nightwatch.json'
+    }));
 });
 
 // bundles javascript for browserify -not currently used
