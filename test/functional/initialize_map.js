fd6225b51a175ea13621309bf9b322ad32f36208
mwytock0812 <michael.wytock@gmail.com>
2016-02-29 12:55:59 -0500

    Configure nightwatch for functional testing.

--- test/functional/initialize_map.js
+++ test/functional/initialize_map.js
@@ -1,11 +1,21 @@
-var assert = require('assert');
+module.exports = {
+  'Test that map initializes ok' : function(client) {
+    client
+      .url('http://localhost:3000')
+      .pause(1000);
 
-describe('Initialize map', function() {
-  it('should load the page properly', function() {
-    assert.ok(true);
-  });
-  it('should load collapsed sidebar panes');
-  it('should display a map centered on the United States');
-  it('should show a universal search bar');
-  it('should display a proper title in the title bar');
-});
+    // expect body element to be present in 1000ms
+    client.expect.element('body').to.be.present.before(1000);
+
+    // expect map to be centered on the United States
+
+    // expect the collapsed sidebar panes to be loaded
+
+    // expect the universal search bar to be loaded
+
+    // expect 'Hurricane Tracker' in the title
+
+
+    client.end();
+  }
+};
